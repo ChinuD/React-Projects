c3e25b75521debacefbe1a91ab8af1274fffd157
ChinuD <dhokeychinmay.com>
2025-01-16 19:21:40 +0530

    second commit

--- To-Do App/src/components/ToDo.jsx
+++ To-Do App/src/components/ToDo.jsx
@@ -1,40 +1,60 @@
-import React, { useState, useRef } from "react";
-import todo_icon from '../assets/todo-app-assets/assets/todo_icon.png'
+import React, { useEffect,useState, useRef } from "react";
+import todo_icon from "../assets/todo-app-assets/assets/todo_icon.png";
 import ToDoItems from "./ToDoItems";
 
 function ToDo() {
+  const [todoList, setToDoList] = useState([]);
 
-    const [todoList, setToDoList] = useState([]);
+  const inputRef = useRef();
 
-    const inputRef = useRef()
-    
-     const add = () => {
-        const inputText = inputRef.current.value.trim();
-        // console.log(inputText)
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    // console.log(inputText)
 
-        if(inputText === ""){
-            return null;
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: true,
+    };
+
+    setToDoList((prev) => [...prev, newTodo]);
+
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setToDoList((prvTodos) => {
+      return prvTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      add();
+    }
+  };
+
+  const toggle = (id) => {
+    setToDoList((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {...todo, isComplete: !todo.isComplete};
         }
+        return todo;
+      });
+    });
+  };
 
-        const newTodo = {
-            id: Date.now(),
-            text:inputText,
-            isComplete: false, 
-        }
-
-        setToDoList((prev) => 
-            [...prev, newTodo]
-        )
-
-        inputRef.current.value = "";
-     }
-
-
-
+  useEffect(()=>{
+    console.log(todoList)
+  },[todoList])
 
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
-
       {/* ------------ title ------------ */}
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={todo_icon} />
@@ -43,19 +63,36 @@ function ToDo() {
 
       {/* ------------ input box --------- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type="text" placeholder="Add your task" />
-        <button onClick={add} className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">Add +</button>
-      </div>
-      
-      
-      {/* ------------ ToDo List --------- */}
-      <div>
-        {todoList.map((item, index)=>{
-            // console.log("Executing")
-            return <ToDoItems key={index} text ={item.text}/>
-        })}
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="Add your task"
+          onKeyPress={handleKeyPress}
+        />
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          Add +
+        </button>
       </div>
 
+      {/* ------------ ToDo List --------- */}
+      <div>
+        {todoList.map((item, index) => {
+          // console.log("Executing")
+          return (
+            <ToDoItems
+              key={index}
+              text={item.text}
+              isComplete={item.isComplete}
+              deleteTodo={() => deleteTodo(item.id)}
+              toggle = {()=> toggle(item.id)} 
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- To-Do App/src/components/ToDoItems.jsx
+++ To-Do App/src/components/ToDoItems.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import tick from '../assets/todo-app-assets/assets/tick.png'
 import notTick from '../assets/todo-app-assets/assets/not_tick.png'
 import delete_icon from '../assets/todo-app-assets/assets/delete.png'
+import ToDo from './ToDo'
 
-const ToDoItems = ({text}) => {
+const ToDoItems = ({text,id,isComplete,deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick}/>
-            <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+        <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+            <img src={isComplete ? tick : notTick}/>
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
         </div>
 
-        <img src={delete_icon} alt="" className='w-3.5 cursor-pointer '/>
+        <img onClick={()=> {deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer '/>
     </div>
   )
 }
